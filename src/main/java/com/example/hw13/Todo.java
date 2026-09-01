cfe0619186bdee2910f404f2ad89daca4945ddfb
Kyrylo Ostashko <148716602+iK1r@users.noreply.github.com>
2026-09-01 15:24:46 +0300

    Create Todo class with basic properties and methods

---
+++ src/main/java/com/example/hw13/Todo.java
@@ -0,0 +1,25 @@
+package com.example.hw13;
+
+public class Todo {
+
+  private int userId;
+  private int id;
+  private String title;
+  private boolean completed;
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+}
